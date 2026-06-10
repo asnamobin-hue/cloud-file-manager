33155e46aacb08fd06f287230b0853b0164da5bb
Asna Mobin <asnamobin@student.iul.ac.in>
2026-06-10 08:19:16 +0000

    Add delete and download features

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import mimetypes
 import boto3
 from dotenv import load_dotenv
 import os
 load_dotenv()
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, send_file
+from io import BytesIO
 app = Flask(__name__)
 
 # AWS S3 Setup
@@ -18,7 +19,12 @@ for bucket in response["Buckets"]:
     print(bucket["Name"])
 @app.route("/")
 def home():
-    return render_template("index.html")
+    response = s3.list_objects_v2(Bucket=BUCKET_NAME)
+    files = []
+    if "Contents" in response:
+      for obj in response["Contents"]:
+         files.append({"name": obj["Key"], "size": obj["Size"], "upload-date": obj["LastModified"], "owner": obj.get("Owner")})
+    return render_template("index.html", files=files)
 
 
 @app.route("/upload", methods=["POST"])
@@ -31,7 +37,17 @@ def upload_file():
         return f"Uploaded: {filename}"
 
     return "No file selected"
-
+@app.route("/delete", methods=["POST"])
+def delete_file():
+    filename = request.form["filename"]
+    s3.delete_object(Bucket=BUCKET_NAME, Key=filename)
+    return f"Deleted: {filename}"  
+@app.route("/download", methods=["GET"])
+def download_file():
+    filename = request.args.get("filename")
+    response = s3.get_object(Bucket=BUCKET_NAME, Key=filename)
+    mime_type, _ = mimetypes.guess_type(filename)
+    return send_file(BytesIO(response["Body"].read()), download_name=filename, as_attachment=True,mimetype=mime_type)
 
 if __name__ == "__main__":
     app.run(debug=True)
